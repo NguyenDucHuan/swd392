0a532033010d92d5dd42006ef466ba7138a53edb
zodicto <minhtan210703@gmail.com>
2025-03-28 04:49:11 +0700

    add some dashboard api

---
+++ swd392_BE/BBSS.Api/ViewModels/FeedbackViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace BBSS.Api.ViewModels
+{
+    public class TotalFeedbacksByMonthViewModel
+    {
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public int TotalFeedbacks { get; set; }
+    }
+
+    public class TotalFeedbacksByQuarterViewModel
+    {
+        public int Quarter { get; set; }
+        public int Year { get; set; }
+        public int TotalFeedbacks { get; set; }
+    }
+
+    public class TotalFeedbacksByYearViewModel
+    {
+        public int Year { get; set; }
+        public int TotalFeedbacks { get; set; }
+    }
+}
